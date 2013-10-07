5a1891ca18704496c942a4f4d9f5e6cd0f0415fa
Itxaka Serrano <iserrano@e-commfactory.com>
2013-10-07 11:21:42 +0200

    modified to upload to pypi

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from setuptools import setup, find_packages
 setup(
-    name = "python-gitlab",
-    version = "0.1",
+    name = "i-python-gitlab",
+    version = "6.1",
     packages = find_packages(),
     install_requires = ['requests', 'markdown'],
     # metadata for upload to PyPI
     author = "Itxaka Serrano Garcia",
     author_email = "itxakaserrano@gmail.com",
-    description = "See the README.md file for more information",
+    description = "Manage gitlab from python",
     license = "GPL3",
     keywords = "gitlab git wrapper",
-    url = "http://github.com/itxaka/python-gitlab/",
+    url = "http://github.com/itxaka/i-python-gitlab/",
 )
