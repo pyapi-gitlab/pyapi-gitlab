3967427e2702cb8b3c44cff996cf7f4618177079
itxaka <iserrano@e-commfactory.com>
2013-10-10 16:58:08 +0200

    fixed description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # metadata for upload to PyPI
     author = "Itxaka Serrano Garcia",
     author_email = "itxakaserrano@gmail.com",
-    description = "See the README.md file for more information",
+    description = "Gitlab API wrapper for Gitlab 6.X",
     license = "GPL3",
     keywords = "gitlab git wrapper",
     url = "http://github.com/itxaka/pyapi-gitlab/",
