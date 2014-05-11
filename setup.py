7171203726e12d49a23a03171056e816ebfe0f2a
Itxaka <itxakaserrano@gmail.com>
2014-05-11 12:28:25 +0200

    bump version to 6.2.3 for pypi package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "pyapi-gitlab",
-    version = "6.2.2",
+    version = "6.2.3",
     packages = find_packages(),
     install_requires = ['requests', 'markdown'],
     # metadata for upload to PyPI
