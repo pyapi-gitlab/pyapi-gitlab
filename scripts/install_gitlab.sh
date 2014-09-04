87f2b80b082f545311a6f233cd353532d601ef61
Tobias Reese <tobias.reese@phimobile.com>
2014-09-04 23:18:14 +0200

    install gitlab

--- scripts/install_gitlab.sh
+++ scripts/install_gitlab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install gitlab
 cd /home/git
-sudo -u git -H git clone https://gitlab.com/gitlab-org/gitlab-ce.git -b 7-2-stable gitlab
+sudo -u git -H git clone --depth=1 https://gitlab.com/gitlab-org/gitlab-ce.git -b 7-2-stable gitlab
 cd /home/git/gitlab
 sudo -u git -H cp config/gitlab.yml.example config/gitlab.yml
 sudo chown -R git log/
@@ -18,3 +18,4 @@ sudo -u git -H cp config/initializers/rack_attack.rb.example config/initializers
 sudo -u git -H git config --global user.name "GitLab"
 sudo -u git -H git config --global user.email "example@example.com"
 sudo -u git -H git config --global core.autocrlf input
+sudo -u git cp config/database.yml.mysql config/database.yml
