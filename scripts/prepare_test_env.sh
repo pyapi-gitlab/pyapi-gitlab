8d5ee3d54e0fe8230ca5fa8e264f550ab0f64e8f
Itxaka Serrano <itxaka.serrano@virdata.com>
2014-10-26 18:42:11 +0100

    travis builds on somewhere that does not provide a init/service system so lets test using system ruby/gem

--- scripts/prepare_test_env.sh
+++ scripts/prepare_test_env.sh
@@ -2,18 +2,20 @@
 # install dependencies
 sudo apt-get update -qq
 # sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
-sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
+#sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
 sudo apt-get install -y git-core
-git --version
+#git --version
 # install ruby
-sudo apt-get remove -y ruby1.8 libruby1.8 libruby1.9.1
-mkdir /tmp/ruby && cd /tmp/ruby
-curl -L --progress ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz | tar xz
-cd ruby-2.1.2
-./configure --disable-install-rdoc
-make
-sudo make install
+#sudo apt-get remove -y ruby1.8 libruby1.8 libruby1.9.1
+#mkdir /tmp/ruby && cd /tmp/ruby
+#curl -L --progress ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz | tar xz
+#cd ruby-2.1.2
+#./configure --disable-install-rdoc
+#make
+#sudo make install
+which gem
 sudo gem install bundler --no-ri --no-rdoc
+which bundle
 # install git user
 sudo adduser --disabled-login --gecos 'GitLab' git
 # install mysql
