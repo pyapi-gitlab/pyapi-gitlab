b284936cc845db3f719c6d674f1a1d14bc1c4ae3
Itxaka Serrano <itxaka.serrano@virdata.com>
2014-10-26 18:27:53 +0100

    pull back to the usual install way, lets create a branch for it

--- scripts/prepare_test_env.sh
+++ scripts/prepare_test_env.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 # install dependencies
-#sudo apt-get update -qq
+sudo apt-get update -qq
 # sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
-#sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
+sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
 sudo apt-get install -y git-core
-#git --version
+git --version
 # install ruby
-#sudo apt-get remove -y ruby1.8 libruby1.8 libruby1.9.1
-#mkdir /tmp/ruby && cd /tmp/ruby
-#curl -L --progress ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz | tar xz
-#cd ruby-2.1.2
-#./configure --disable-install-rdoc
-#make
-#sudo make install
-#curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
-#source /home/travis/.rvm/scripts/rvm
-#sudo gem install bundler --no-ri --no-rdoc
+sudo apt-get remove -y ruby1.8 libruby1.8 libruby1.9.1
+mkdir /tmp/ruby && cd /tmp/ruby
+curl -L --progress ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz | tar xz
+cd ruby-2.1.2
+./configure --disable-install-rdoc
+make
+sudo make install
+sudo gem install bundler --no-ri --no-rdoc
 # install git user
 sudo adduser --disabled-login --gecos 'GitLab' git
 # install mysql
