98d2a8546cc6b6e7955b40f607321579fd9aedd5
Itxaka Serrano <itxaka.serrano@virdata.com>
2014-10-26 18:16:04 +0100

    try to use system pre-installed ruby?

--- scripts/prepare_test_env.sh
+++ scripts/prepare_test_env.sh
@@ -13,8 +13,8 @@ sudo apt-get install -y git-core
 #./configure --disable-install-rdoc
 #make
 #sudo make install
-curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
-source /home/travis/.rvm/scripts/rvm
+#curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
+#source /home/travis/.rvm/scripts/rvm
 sudo gem install bundler --no-ri --no-rdoc
 # install git user
 sudo adduser --disabled-login --gecos 'GitLab' git
