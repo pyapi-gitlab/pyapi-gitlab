cfb722bfa70e9ff97ba27f7da93328c5e913c6e1
Itxaka Serrano <itxaka.serrano@virdata.com>
2014-10-26 18:32:32 +0100

    test the omnibus package for gitlab install

---
+++ scripts/install_gitlab_omnibus.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install openssh-server sendmail
+wget https://downloads-packages.s3.amazonaws.com/ubuntu-12.04/gitlab_7.4.2-omnibus-1_amd64.deb
+sudo dpkg -i gitlab_7.4.2-omnibus-1_amd64.deb
+echo 'external_url "http://localhost:8080' | sudo tee /etc/gitlab/gitlab.rb
+sudo gitlab-ctl reconfigure
