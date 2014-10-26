c834d1ca811183cabcfcf573db5eb38a71c350af
Itxaka Serrano <itxaka.serrano@virdata.com>
2014-10-26 19:15:14 +0100

    just use travis user for installing gitlab

--- scripts/install_gitlab.sh
+++ scripts/install_gitlab.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 # install gitlab
-cd /home/git
-sudo -u git -H git clone --depth=1 https://gitlab.com/gitlab-org/gitlab-ce.git -b 7-2-stable gitlab
-cd /home/git/gitlab
-sudo -u git -H sh -c "cat config/gitlab.yml.example | sed 's/port: 80/port: 8080/g' > config/gitlab.yml"
-sudo chown -R git log/
-sudo chown -R git tmp/
-sudo chmod -R u+rwX log/
-sudo chmod -R u+rwX tmp/
-sudo -u git -H mkdir /home/git/gitlab-satellites
-sudo chmod u+rwx,g=rx,o-rwx /home/git/gitlab-satellites
-sudo chmod -R u+rwX tmp/pids/
-sudo chmod -R u+rwX tmp/sockets/
-sudo chmod -R u+rwX  public/uploads
-sudo -u git -H cp config/unicorn.rb.example config/unicorn.rb
-sudo -u git -H cp config/initializers/rack_attack.rb.example config/initializers/rack_attack.rb
-sudo -u git -H git config --global user.name "GitLab"
-sudo -u git -H git config --global user.email "example@example.com"
-sudo -u git -H git config --global core.autocrlf input
-sudo -u git -H sh -c "cat config/database.yml.mysql | sed 's/password: \".*\"/password: "git"/g' > config/database.yml"
-sudo -u git -H chmod o-rwx config/database.yml
-bundle -v
-sudo -u git -H bundle install --deployment --without development test postgres aws
-sudo -u git -H bundle install --deployment --without development test postgres aws
+git clone --depth=1 https://gitlab.com/gitlab-org/gitlab-ce.git -b 7-2-stable gitlab
+cd /home/travis/gitlab
+sh -c "cat config/gitlab.yml.example | sed 's/port: 80/port: 8080/g' > config/gitlab.yml"
+chmod -R u+rwX log/
+chmod -R u+rwX tmp/
+mkdir /home/travis/gitlab-satellites
+chmod u+rwx,g=rx,o-rwx /home/travis/gitlab-satellites
+chmod -R u+rwX tmp/pids/
+chmod -R u+rwX tmp/sockets/
+chmod -R u+rwX public/uploads
+cp config/unicorn.rb.example config/unicorn.rb
+cp config/initializers/rack_attack.rb.example config/initializers/rack_attack.rb
+git config --global user.name "GitLab"
+git config --global user.email "example@example.com"
+git config --global core.autocrlf input
+sh -c "cat config/database.yml.mysql | sed 's/password: \".*\"/password: "git"/g' > config/database.yml"
+chmod o-rwx config/database.yml
+bundle install --deployment --without development test postgres aws
+bundle install --deployment --without development test postgres aws
 # install gitlab shell
-sudo -u git -H bundle exec rake gitlab:shell:install[v1.9.7] REDIS_URL=redis://localhost:6379 RAILS_ENV=production
-echo "yes" | sudo -u git -H bundle exec rake gitlab:setup RAILS_ENV=production
-sudo cp lib/support/init.d/gitlab /etc/init.d/gitlab
-sudo update-rc.d gitlab defaults 21
-sudo -u git -H bundle exec rake gitlab:env:info RAILS_ENV=production
-sudo -u git -H bundle exec rake assets:precompile RAILS_ENV=production
-sudo service gitlab restart
-sudo -u git -H bundle exec rake gitlab:check RAILS_ENV=production
+bundle exec rake gitlab:shell:install[v1.9.7] REDIS_URL=redis://localhost:6379 RAILS_ENV=production
+echo "yes" |bundle exec rake gitlab:setup RAILS_ENV=production
+cp lib/support/init.d/gitlab /etc/init.d/gitlab
+update-rc.d gitlab defaults 21
+bundle exec rake gitlab:env:info RAILS_ENV=production
+bundle exec rake assets:precompile RAILS_ENV=production
+service gitlab restart
+bundle exec rake gitlab:check RAILS_ENV=production
 
--- scripts/prepare_test_env.sh
+++ scripts/prepare_test_env.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 # install dependencies
 sudo apt-get update -qq
-# sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
-#sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate python-docutils pkg-config cmake
 sudo apt-get install -y git-core
-#git --version
 # install ruby
-#sudo apt-get remove -y ruby1.8 libruby1.8 libruby1.9.1
-#mkdir /tmp/ruby && cd /tmp/ruby
-#curl -L --progress ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz | tar xz
-#cd ruby-2.1.2
-#./configure --disable-install-rdoc
-#make
-#sudo make install
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+gem install bundler
 # install git user
 sudo adduser --disabled-login --gecos 'GitLab' git
-sudo curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
-which gem
-sudo rvm explode
 # install mysql
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server mysql-client libmysqlclient-dev
 mysql --version
