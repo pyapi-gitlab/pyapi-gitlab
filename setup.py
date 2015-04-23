2cb3483b52afc2ad2049d17c31e966d9ff4e3ec2
Itxaka <itxakaserrano@gmail.com>
2015-04-24 00:04:03 +0200

    Should have updated the version number to reflect that it supports up to 7.8.x

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "pyapi-gitlab",
-    version = "7.5.4",
+    version = "7.8.4",
     packages = find_packages(),
     install_requires = ['requests'],
     extras_require = {
