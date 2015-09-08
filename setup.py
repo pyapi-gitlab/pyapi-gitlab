c1fbcd638519bf8f7c8271ffa921e830f8e2ec2d
Itxaka <itxakaserrano@gmail.com>
2015-09-08 21:27:14 +0200

    bump version for future release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "pyapi-gitlab",
-    version = "7.8.5",
+    version = "7.8.6",
     packages = find_packages(),
     install_requires = ['requests'],
     extras_require = {
