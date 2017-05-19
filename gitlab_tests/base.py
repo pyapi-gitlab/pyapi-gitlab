09c33b9dd7cc686857d1a8b1e37cd0205ff7ff4c
Emett Speer <speer.emett@gmail.com>
2017-05-18 18:00:58 -0700

    Updated root password for gitlab

--- gitlab_tests/base.py
+++ gitlab_tests/base.py
@@ -10,7 +10,7 @@ class BaseTest(unittest.TestCase):
     @responses.activate
     def setUp(self):
         self.user = os.environ.get('gitlab_user', 'root')
-        self.password = os.environ.get('gitlab_password', '5iveL!fe')
+        self.password = os.environ.get('gitlab_password', '1WmA1a3ONs9F')
         self.host = os.environ.get('gitlab_host', 'http://localhost:10080')
         self.gitlab = Gitlab(host=self.host, verify_ssl=False)
         self.gitlab.host = self.host
--- integration_tests/base.py
+++ integration_tests/base.py
@@ -12,7 +12,8 @@ def log_to_term(var_name, response):
 class BaseTest(TestCase):
     def setUp(self):
         self.user = os.environ.get('gitlab_user', 'root')
-        self.password = os.environ.get('gitlab_password', '5iveL!fe')
+        self.password = os.environ.get('gitlab_password', '1WmA1a3ONs9F')
         self.host = os.environ.get('gitlab_host', 'http://localhost:10080')
         self.gitlab = Gitlab(host=self.host, verify_ssl=False)
+
         self.gitlab.login(user=self.user, password=self.password)
