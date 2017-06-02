2d7c8ffb84665153db021a9cda7a3973d82b6564
Emett Speer <speer.emett@gmail.com>
2017-06-02 10:57:13 -0700

    Updates for jenkins integration runner

--- integration_tests/base.py
+++ integration_tests/base.py
@@ -6,13 +6,16 @@ from gitlab import Gitlab
 
 
 def log_to_term(var_name, response):
-    print('\r\r{}: {}\r\r'.format(var_name, response))
+    try:
+        print('\r\r{}: {}\r\r'.format(var_name, response.keys()))
+    except:
+        print('\r\r{}: {}\r\r'.format(var_name, response))
 
 
 class BaseTest(TestCase):
     def setUp(self):
         self.user = os.environ.get('gitlab_user', 'root')
-        self.password = os.environ.get('gitlab_password', '1WmA1a3ONs9F')
+        self.password = os.environ.get('gitlab_password', '5iveL!fe')
         self.host = os.environ.get('gitlab_host', 'http://localhost:10080')
         self.gitlab = Gitlab(host=self.host, verify_ssl=False)
 
--- integration_tests/tests_v91/tests_users.py
+++ integration_tests/tests_v91/tests_users.py
@@ -1,18 +1,14 @@
 from integration_tests.base import BaseTest, log_to_term
+from response_data.users import *
 
 
 class TestGitlabV91(BaseTest):
     def test_add_remove_user(self):
         """Add and remove a user"""
-        response = self.gitlab.edituser(1, password='1WmA1a3ONs9F')
-        log_to_term('update_password:', response)
-        self.assertTrue(response)
-
         response = self.gitlab.createuser(
             'test', 'test', 'TestPassword1', 'test@example.com')
-        log_to_term('create_user', response)
+        self.assertEqual(post_users.keys(), response.keys())
 
         response = self.gitlab.delete_user(response['id'])
-        log_to_term('delete_user', response)
         self.assertTrue(isinstance(response, dict))
         self.assertEqual({}, response)
