70296966db79551f7ce466d6e7015fbb6393e871
Emett Speer <speer.emett@gmail.com>
2017-07-24 15:16:08 -0700

    Updated integration tests and travis ci file

--- integration_tests/base.py
+++ integration_tests/base.py
@@ -7,17 +7,13 @@ from gitlab import Gitlab
 
 def log_to_term(var_name, response):
     print('\r\r', var_name, ':', response, '\r\r')
-    # try:
-    #     print('\r\n\r\n{}: {}\r\n\r\n'.format(var_name, response.keys()))
-    # except:
-    #     print('\r\r{}: {}\r\r'.format(var_name, response))
 
 
 class BaseTest(TestCase):
     def setUp(self):
         self.user = os.environ.get('gitlab_user', 'root')
         self.password = os.environ.get('gitlab_password', '5iveL!fe')
-        self.host = os.environ.get('gitlab_host', 'http://gitlab:80')
+        self.host = os.environ.get('gitlab_host', 'http://gitlab:20')
 
         self.gitlab = Gitlab(host=self.host, verify_ssl=False)
 
