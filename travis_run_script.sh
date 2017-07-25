faa95daea55387ce55c723a6d96d0b60fab869e6
Emett Speer <speer.emett@gmail.com>
2017-07-24 19:58:47 -0700

    Testing matrix

--- travis_run_script.sh
+++ travis_run_script.sh
@@ -2,7 +2,6 @@
 if $INTEGRATION_TEST -eq 1; then
     docker-compose -f docker/v93/docker-compose.yml build
     docker-compose -f docker/v93/docker-compose.yml up --abort-on-container-exit
-    ./cov
 else
     pip install -Ur test-requirements.txt
     ./cov
